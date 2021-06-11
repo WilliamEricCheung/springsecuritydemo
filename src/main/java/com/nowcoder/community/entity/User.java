e1297b3be105edb22de916af69b69a80a6a6ad24
William Eric Cheung <williamericcheung@outlook.com>
2021-06-11 13:53:21 +0800

    学习

--- src/main/java/com/nowcoder/community/entity/User.java
+++ src/main/java/com/nowcoder/community/entity/User.java
@@ -1,8 +1,14 @@
 package com.nowcoder.community.entity;
 
-import java.util.Date;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+
+public class User implements UserDetails {
 
     private int id;
     private String username;
@@ -27,8 +33,49 @@ public class User {
         return username;
     }
 
+    // 账号未国企
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    // 账号未锁定
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    // 凭证未过期
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    // 账号可用
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> list = new ArrayList<>();
+        list.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                switch (type){
+                    case 1:
+                        return "ADMIN";
+                    default:
+                        return "USER";
+                }
+            }
+        });
+        return null;
     }
 
     public String getPassword() {
